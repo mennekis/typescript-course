cbf4364cca840d5936a5b676f97be14c4a48f23a
Oleksiy Nikolayenko <mennekis@gmail.com>
2023-10-05 17:28:58 +0300

    Nikolayenko I am in stuck with tests

--- src/lesson11-homework-test.ts
+++ src/lesson11-homework-test.ts
@@ -1,254 +1,269 @@
 "use strict";
 
+import { data } from "./users-data";
+import { Users } from "./lesson11-homework";
+import { expect } from "chai";
+
 declare const chai: any;
 declare const describe: any;
 declare const it: any;
 declare const xit: any;
 
 describe("Users class", () => {
-  const { expect } = chai;
+   let users: Users;
 
-  it("should be defined", () => {
-    expect(Users).to.not.be.undefined;
-  });
+   it("should be defined", () => {
+      const users = new Users(data.users);
+      expect(users).to.not.be.undefined;
+   });
 
-  it("should define constructor that accepts users list", () => {
-    const users = new Users(data.users);
-
-    expect(users.list).to.equal(data.users);
-  });
-
-  describe("getUsersNames method", () => {
-    // - Написати метод що повератє масив імен усіх користувачів у форматі "John, Smith" ("firstName, lastName")
-
-    it("should be defined", () => {
+   it("should define constructor that accepts users list", () => {
       const users = new Users(data.users);
 
-      expect(users.getUsersNames).to.not.be.undefined;
-    });
+      expect(users.list).to.equal(data.users);
+   });
 
-    it("should return list of user names", () => {
-      const mockData = [
-        { firstName: "Bob", lastName: "Smith" },
-        { firstName: "Jack", lastName: "Chan" },
-      ];
+   describe("getUsersNames method", () => {
+      // - Написати метод що повератє масив імен усіх користувачів у форматі "John, Smith" ("firstName, lastName")
 
-      const users = new Users(mockData);
+      it("should be defined", () => {
+         const users = new Users(data.users);
 
-      expect(users.getUsersNames()).to.deep.equal(["Bob, Smith", "Jack, Chan"]);
-    });
-
-    xit("should work for blank data", () => {
-      const users = new Users([]);
-
-      // - Виправити getUsersNames так, що цей юніт-тест проходив
-      expect(users.getUsersNames()).to.deep.equal([]);
-    });
-
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
-
-      // - Дописати цей юніт-тест, так щоб використовувалися data.users
-      expect(users.getUsersNames()).to.deep.equal(["Terry, Medhurst", "..."]);
-    });
-  });
-
-  describe("updateUsersAge method", () => {
-    // - Написати метод що виправляє вік користувачів, апдейтить дані користувачів правильним віком (age) на поточну дату, 2023 рік (використати день народження birthDate для обчислення віку)
-
-    it("should be defined", () => {
-      const users = new Users(data.users);
-
-      expect(users.updateUsersAge).to.not.be.undefined;
-    });
-
-    it("should update users age", () => {
-      const mockData = [
-        {
-          firstName: "Bob",
-          lastName: "Smith",
-          age: 18,
-          birthDate: "2003-02-13",
-        },
-        {
-          firstName: "Jack",
-          lastName: "Chan",
-          age: 28,
-          birthDate: "1993-02-13",
-        },
-      ];
-
-      const users = new Users(mockData);
-
-      users.updateUsersAge();
-
-      const usersAges = users.list.map((user) => user.age);
-
-      expect(usersAges).to.deep.equal([20, 30]);
-    });
-
-    xit("should work for blank data", () => {
-      const users = new Users([]);
-
-      users.updateUsersAge();
-
-      const usersAges = users.list.map((user) => user.age);
-
-      // - Виправити updateUsersAge так, що цей юніт-тест проходив
-      expect(usersAges).to.deep.equal([]);
-    });
-
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
-
-      users.updateUsersAge();
-
-      const usersAges = users.list.map((user) => user.age);
-
-      // - Виправити updateUsersAge так, що цей юніт-тест проходив
-      // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 1, 2, 3 на правильні дані
-      expect(usersAges).to.deep.equal([1, 2, 3]);
-    });
-  });
-
-  describe("getUsersFromUkraine method", () => {
-    // - Написати функцію що повератє список користувачів з України (номер телефону +380).
-
-    it("should be defined", () => {
-      const users = new Users(data.users);
-
-      expect(users.getUsersFromUkraine).to.not.be.undefined;
-    });
-
-    it("should return list of users from Ukraine", () => {
-      const mockData = [
-        { firstName: "Bob", lastName: "Smith", phone: "+7 813 117 7139" },
-        { firstName: "Jack", lastName: "Chan", phone: "+7 813 117 7139" },
-        { firstName: "Ove", lastName: "Malyk", phone: "+3809525426549" },
-        { firstName: "Myk", lastName: "Franko", phone: "+3809625426549" },
-      ];
-
-      const users = new Users(mockData);
-
-      const usersNamesAndPhones = users
-        .getUsersFromUkraine()
-        .map(({ firstName, lastName, phone }) => ({
-          firstName,
-          lastName,
-          phone,
-        }));
-
-      expect(usersNamesAndPhones.length).to.equal(2);
-
-      expect(usersNamesAndPhones).to.deep.include({
-        firstName: "Myk",
-        lastName: "Franko",
-        phone: "+3809625426549",
+         expect(users.getUsersNames).to.not.be.undefined;
       });
 
-      expect(usersNamesAndPhones).to.deep.include({
-        firstName: "Ove",
-        lastName: "Malyk",
-        phone: "+3809525426549",
+      it("should return list of user names", () => {
+         const mockData = [
+            { firstName: "Bob", lastName: "Smith" },
+            { firstName: "Jack", lastName: "Chan" },
+         ];
+
+         const users = new Users(mockData);
+
+         expect(users.getUsersNames()).to.deep.equal([
+            "Bob, Smith",
+            "Jack, Chan",
+         ]);
       });
-    });
 
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
+      xit("should work for blank data", () => {
+         const users = new Users([]);
 
-      const usersNamesAndPhones = users
-        .getUsersFromUkraine()
-        .map(({ firstName, lastName, phone }) => ({
-          firstName,
-          lastName,
-          phone,
-        }));
-
-      expect(usersNamesAndPhones.length).to.equal(1);
-
-      // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
-      expect(usersNamesAndPhones).to.deep.include({
-        firstName: "Assunta",
-        lastName: "Rath",
-        phone: "+380 962 542 6549",
+         // - Виправити getUsersNames так, що цей юніт-тест проходив
+         expect(users.getUsersNames()).to.deep.equal([]);
       });
-    });
-  });
 
-  describe("getStatePostalCodes method", () => {
-    // - Написати функцію що на основі даних усіх користувачів повератє масив штатів і їх поштових індексів у вигляді масиву обєктів [{"name": "AK", "postalCodes": ["99503", "..."]}]. Штати не повинні повторюватися.
+      xit("should work for real data", () => {
+         const users = new Users(data.users);
 
-    it("should be defined", () => {
-      const users = new Users(data.users);
+         // - Дописати цей юніт-тест, так щоб використовувалися data.users
+         expect(users.getUsersNames()).to.deep.equal([
+            "Terry, Medhurst",
+            "...",
+         ]);
+      });
+   });
 
-      expect(users.getStatePostalCodes).to.not.be.undefined;
-    });
+   describe("updateUsersAge method", () => {
+      // - Написати метод що виправляє вік користувачів, апдейтить дані користувачів правильним віком (age) на поточну дату, 2023 рік (використати день народження birthDate для обчислення віку)
 
-    it("should return list of postal codes for all states", () => {
-      const mockData = [
-        {
-          firstName: "Joe",
-          address: {
-            postalCode: "80003",
-            state: "CO",
-          },
-        },
-        {
-          firstName: "Bob",
-          address: {
-            postalCode: "80004",
-            state: "CO",
-          },
-        },
-        {
-          firstName: "Mike",
-          address: {
-            postalCode: "10001",
-            state: "AK",
-          },
-        },
-      ];
+      it("should be defined", () => {
+         const users = new Users(data.users);
 
-      const users = new Users(mockData);
+         expect(users.updateUsersAge).to.not.be.undefined;
+      });
 
-      expect(users.getStatePostalCodes()).to.deep.equal([
-        { name: "CO", postalCodes: ["80003", "80004"] },
-        { name: "AK", postalCodes: ["10001"] },
-      ]);
-    });
+      it("should update users age", () => {
+         const mockData = [
+            {
+               firstName: "Bob",
+               lastName: "Smith",
+               age: 18,
+               birthDate: "2003-02-13",
+            },
+            {
+               firstName: "Jack",
+               lastName: "Chan",
+               age: 28,
+               birthDate: "1993-02-13",
+            },
+         ];
 
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
+         const users = new Users(mockData);
 
-      // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
-      // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити XXX на правильні дані
-      expect(users.getStatePostalCodes()).to.deep.equal([
-        { name: "XXX", postalCodes: ["XXX", "XXX"] },
-        { name: "XXX", postalCodes: ["XXX", "XXX"] },
-        // ... Це місце де має бути ваш код
-      ]);
-    });
-  });
+         users.updateUsersAge();
 
-  describe("getAverageWomenAge method", () => {
-    // - Написати функцію що повератє середній вік всіх жінок ("gender": "female")
+         const usersAges = users.list.map((user) => user.age);
 
-    xit("should be defined", () => {
-      const users = new Users(data.users);
+         expect(usersAges).to.deep.equal([20, 30]);
+      });
 
-      // expect(users.getMediumWomenAge).to.not.be.undefined;
-    });
+      xit("should work for blank data", () => {
+         const users = new Users([]);
 
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
+         users.updateUsersAge();
 
-      // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
-      // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 12345 на правильні дані
-      // expect(users.getMediumWomenAge()).to.deep.equal(12345);
-    });
-  });
+         const usersAges = users.list.map((user) => user.age);
 
-  // Написати юніт-тести для наступних методів
-  // getMostCommonWoomanHairColor - метод що повератє найбільш поширений колір волося серед жінок
-  // getMostCommonManBlodType - метод що повератє найбільш поширениу групу крові серед чоловіків
+         // - Виправити updateUsersAge так, що цей юніт-тест проходив
+         expect(usersAges).to.deep.equal([]);
+      });
+
+      xit("should work for real data", () => {
+         const users = new Users(data.users);
+
+         users.updateUsersAge();
+
+         const usersAges = users.list.map((user) => user.age);
+
+         // - Виправити updateUsersAge так, що цей юніт-тест проходив
+         // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 1, 2, 3 на правильні дані
+         expect(usersAges).to.deep.equal([1, 2, 3]);
+      });
+   });
+
+   describe("getUsersFromUkraine method", () => {
+      // - Написати функцію що повератє список користувачів з України (номер телефону +380).
+
+      it("should be defined", () => {
+         const users = new Users(data.users);
+
+         expect(users.getUsersFromUkraine).to.not.be.undefined;
+      });
+
+      it("should return list of users from Ukraine", () => {
+         const mockData = [
+            { firstName: "Bob", lastName: "Smith", phone: "+7 813 117 7139" },
+            { firstName: "Jack", lastName: "Chan", phone: "+7 813 117 7139" },
+            { firstName: "Ove", lastName: "Malyk", phone: "+3809525426549" },
+            { firstName: "Myk", lastName: "Franko", phone: "+3809625426549" },
+         ];
+
+         const users = new Users(mockData);
+
+         const usersNamesAndPhones = users
+            .getUsersFromUkraine()
+            .map(({ firstName, lastName, phone }) => ({
+               firstName,
+               lastName,
+               phone,
+            }));
+
+         expect(usersNamesAndPhones.length).to.equal(2);
+
+         expect(usersNamesAndPhones).to.deep.include({
+            firstName: "Myk",
+            lastName: "Franko",
+            phone: "+3809625426549",
+         });
+
+         expect(usersNamesAndPhones).to.deep.include({
+            firstName: "Ove",
+            lastName: "Malyk",
+            phone: "+3809525426549",
+         });
+      });
+
+      xit("should work for real data", () => {
+         const users = new Users(data.users);
+
+         const usersNamesAndPhones = users
+            .getUsersFromUkraine()
+            .map(({ firstName, lastName, phone }) => ({
+               firstName,
+               lastName,
+               phone,
+            }));
+
+         expect(usersNamesAndPhones.length).to.equal(1);
+
+         // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
+         expect(usersNamesAndPhones).to.deep.include({
+            firstName: "Assunta",
+            lastName: "Rath",
+            phone: "+380 962 542 6549",
+         });
+      });
+   });
+
+   describe("getStatePostalCodes method", () => {
+      // - Написати функцію що на основі даних усіх користувачів повератє масив штатів і їх поштових індексів у вигляді масиву обєктів [{"name": "AK", "postalCodes": ["99503", "..."]}]. Штати не повинні повторюватися.
+
+      it("should be defined", () => {
+         const users = new Users(data.users);
+
+         expect(users.getStatePostalCodes).to.not.be.undefined;
+      });
+
+      it("should return list of postal codes for all states", () => {
+         const mockData = [
+            {
+               firstName: "Joe",
+               address: {
+                  postalCode: "80003",
+                  state: "CO",
+               },
+            },
+            {
+               firstName: "Bob",
+               address: {
+                  postalCode: "80004",
+                  state: "CO",
+               },
+            },
+            {
+               firstName: "Mike",
+               address: {
+                  postalCode: "10001",
+                  state: "AK",
+               },
+            },
+         ];
+
+         const users = new Users(mockData);
+
+         expect(users.getStatePostalCodes()).to.deep.equal([
+            { name: "CO", postalCodes: ["80003", "80004"] },
+            { name: "AK", postalCodes: ["10001"] },
+         ]);
+      });
+
+      xit("should work for real data", () => {
+         const users = new Users(data.users);
+
+         // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
+         // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити XXX на правильні дані
+         expect(users.getStatePostalCodes()).to.deep.equal([
+            { name: "XXX", postalCodes: ["XXX", "XXX"] },
+            { name: "XXX", postalCodes: ["XXX", "XXX"] },
+            // ... Це місце де має бути ваш код
+         ]);
+      });
+   });
+
+   describe("getAverageWomenAge method", () => {
+      // - Написати функцію що повератє середній вік всіх жінок ("gender": "female")
+      it("should calculate the average age of female users", () => {
+         const users = new Users(userData);
+         const averageAge = users.getAverageWomenAge();
+         // Ожидаем, что средний возраст женщин равен 30, так как в вашем тестовом наборе данных у обеих женщин возраст 30
+         expect(averageAge).to.equal(30);
+      xit("should be defined", () => {
+         const users = new Users(data.users);
+
+         // expect(users.getMediumWomenAge).to.not.be.undefined;
+      });
+
+      xit("should work for real data", () => {
+         const users = new Users(data.users);
+
+         // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
+         // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 12345 на правильні дані
+         // expect(users.getMediumWomenAge()).to.deep.equal(12345);
+      });
+   });
+
+   // Написати юніт-тести для наступних методів
+   // getMostCommonWoomanHairColor - метод що повератє найбільш поширений колір волося серед жінок
+   // getMostCommonManBlodType - метод що повератє найбільш поширениу групу крові серед чоловіків
 });
--- src/lesson11-homework.ts
+++ src/lesson11-homework.ts
@@ -1,59 +1,119 @@
-class Users {
-  // TODO: Add type for list property, remove `any` type annotation
-  constructor(public list: any[]) {}
+import { data } from "./users-data";
 
-  // TODO: Write method that returns array of users names in format "John, Smith" ("firstName, lastName")
-  // TODO: Need to fix code to use this.list property data
-  getUsersNames() {
-    return ["Bob, Smith", "Jack, Chan"];
-  }
-
-  // TODO: Write method that updates users age to current date (2023 year)
-  // TODO: Need to fix code to use this.list property data
-  // TODO: Need to not mutate this.list property, but return new array of users with updated age
-  updateUsersAge() {
-    this.list = [
-      {
-        firstName: "Bob",
-        lastName: "Smith",
-        age: 20,
-        birthDate: "2003-02-13",
-      },
-      {
-        firstName: "Jack",
-        lastName: "Chan",
-        age: 30,
-        birthDate: "1993-02-13",
-      },
-    ];
-  }
-
-  // TODO: Implement method that returns users from Ukraine (phone number +380)
-  // TODO: Use this.list property data
-  getUsersFromUkraine() {
-    return [
-      {
-        firstName: "Ove",
-        lastName: "Malyk",
-        phone: "+3809525426549",
-        age: 30,
-        birthDate: "1993-02-13",
-      },
-      {
-        firstName: "Myk",
-        lastName: "Franko",
-        phone: "+3809625426549",
-        age: 30,
-        birthDate: "1993-02-13",
-      },
-    ];
-  }
-
-  // TODO: Implement method that returns postal codes grouped by state, using this.list user data
-  getStatePostalCodes() {
-    return [
-      { name: "CO", postalCodes: ["80003", "80004"] },
-      { name: "AK", postalCodes: ["10001"] },
-    ];
-  }
+interface UserData {
+   firstName: string;
+   lastName: string;
+   age: number;
+   birthDate: string;
+   phone: string;
+   [key: string]: string | number;
 }
+
+export class Users {
+   // TODO: Add type for list property, remove `any` type annotation
+   public list: UserData[];
+   constructor(list: UserData[]) {
+      this.list = list;
+   }
+
+   // TODO: Write method that returns array of users names in format "John, Smith" ("firstName, lastName")
+   // TODO: Need to fix code to use this.list property data
+   getUsersNames(): string[] {
+      return this.list.map((user) => `${user.firstName}, ${user.lastName}`);
+   }
+
+   // TODO: Write method that updates users age to current date (2023 year)
+   // TODO: Need to fix code to use this.list property data
+   // TODO: Need to not mutate this.list property, but return new array of users with updated age
+   updateUsersAge(): UserData[] {
+      const currentYear = new Date().getFullYear();
+      this.list.forEach((user) => {
+         user.age = currentYear - new Date(user.birthDate).getFullYear();
+      });
+      return this.list;
+   }
+
+   // TODO: Implement method that returns users from Ukraine (phone number +380)
+   // TODO: Use this.list property data
+   getUsersFromUkraine(): UserData[] {
+      return this.list.filter((user) => user.phone.startsWith("+380"));
+   }
+
+   // TODO: Implement method that returns postal codes grouped by state, using this.list user data
+   getStatePostalCodes(): { name: string; postalCodes: string[] }[] {
+      const stateMap: { [key: string]: string[] } = {};
+      this.list.forEach((user) => {
+         const state = user.phone.substring(1, 3);
+         if (!stateMap[state]) {
+            stateMap[state] = [];
+         }
+         stateMap[state].push(user.phone.substring(3));
+      });
+      return Object.keys(stateMap).map((state) => ({
+         name: state,
+         postalCodes: stateMap[state],
+      }));
+   }
+   getAverageWomenAge(): number {
+      const women = this.list.filter((user) => user.gender === "female");
+      const totalAge = women.reduce((sum, user) => sum + user.age, 0);
+      return totalAge / women.length;
+   }
+   getMostCommonWoomanHairColor(): string {
+      const women = this.list.filter((user) => user.gender === "female");
+      const hairColorCounts: { [key: string]: number } = {};
+      women.forEach((user) => {
+         const color = user.hairColor;
+         hairColorCounts[color] = (hairColorCounts[color] || 0) + 1;
+      });
+      const mostCommonColor = Object.keys(hairColorCounts).reduce((a, b) =>
+         hairColorCounts[a] > hairColorCounts[b] ? a : b
+      );
+      return mostCommonColor;
+   }
+   getMostCommonManBlodType(): string {
+      const men = this.list.filter((user) => user.gender === "male");
+      const bloodTypeCounts: { [key: string]: number } = {};
+      men.forEach((user) => {
+         const bloodType = user.bloodType;
+         bloodTypeCounts[bloodType] = (bloodTypeCounts[bloodType] || 0) + 1;
+      });
+      const mostCommonBloodType = Object.keys(bloodTypeCounts).reduce((a, b) =>
+         bloodTypeCounts[a] > bloodTypeCounts[b] ? a : b
+      );
+      return mostCommonBloodType;
+   }
+}
+
+export const userData: UserData[] = [
+   {
+      firstName: "Ove",
+      lastName: "Malyk",
+      age: 30,
+      birthDate: "1993-02-13",
+      phone: "+3809525426549",
+      gender: "male",
+      hairColor: "bold",
+      bloodType: "A=",
+   },
+   {
+      firstName: "Myk",
+      lastName: "Franko",
+      age: 30,
+      birthDate: "1993-02-13",
+      phone: "+3809625426549",
+      gender: "female",
+      hairColor: "blond",
+      bloodType: "AB",
+   },
+];
+
+export const users = new Users(userData);
+
+console.log(users.getUsersNames());
+console.log(users.updateUsersAge());
+console.log(users.getUsersFromUkraine());
+console.log(users.getStatePostalCodes());
+console.log(users.getAverageWomenAge());
+console.log(users.getMostCommonWoomanHairColor());
+console.log(users.getMostCommonManBlodType());
